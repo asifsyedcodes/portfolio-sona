7af4c76a0a633974434e8d460ed3676b8dad15d4
Asif Syed <asiifsyed@gmail.com>
2025-02-16 21:05:13 -0800

    added Location

--- src/app/(main)/about/AboutSection/index.tsx
+++ src/app/(main)/about/AboutSection/index.tsx
@@ -23,7 +23,6 @@ export const AboutSection = () => {
             <span className="text-green-700 font-libre">SONA</span>{" "}
             <span className="text-green-900 font-libre">SATHAR</span>
           </h1>
-
           <p className="mt-4 text-lg md:text-xl font-bold text-gray-800 text-center lg:text-left">
             I am a Learner’s Designer by intention.
           </p>
@@ -52,6 +51,9 @@ export const AboutSection = () => {
             I am always eager for opportunities to design learning experiences
             that help both learners and myself grow and be better at what we do.
             Let’s connect if this resonates with you!
+          </p>
+          <p className="text-base italic mt-3 text-gray-700">
+            — Based in Vancouver, Canada
           </p>
         </div>
       </div>
