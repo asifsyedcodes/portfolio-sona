b180b45ffaade3cde0ae7d22c4930ec22f2a2b36
Asif Syed <asiifsyed@gmail.com>
2025-02-16 20:58:15 -0800

    favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,6 +32,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.ico" />
+      </head>
       <body
         className={`${lato.variable} ${adventPro.variable} ${libreBaskerville.variable} antialiased`}
       >
