18f2d982b8f33dc49b0a67aab10963d6b5d144a3
Asif Syed <asiifsyed@gmail.com>
2025-02-17 00:18:22 -0800

    padding bottom

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -7,7 +7,7 @@ export default function MainLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <div className="min-h-screen flex flex-col">
+    <div className="min-h-screen flex flex-col pb-16">
       <Header />
       <main className="flex-grow">{children}</main>
       <Footer />
