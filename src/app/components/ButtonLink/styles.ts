7012c7aac4b06b89b13920e942701a0df442a504
Asif Syed <asiifsyed@gmail.com>
2025-02-16 19:22:50 -0800

    outline button color change

--- src/app/components/ButtonLink/styles.ts
+++ src/app/components/ButtonLink/styles.ts
@@ -1,4 +1,5 @@
 export const variantStyles = {
   primary: "border-green-700 bg-green-700 text-white-900 hover:bg-green-900",
-  outline: "border-green-200 bg-green-300 text-black-900 hover:bg-green-200",
+  outline:
+    "border-black-900 bg-green-300 text-black-900 hover:bg-green-200 hover:border-green-200",
 };
