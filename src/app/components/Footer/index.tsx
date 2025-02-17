204d266be972171f9c480d8c8bfc386ea41bddf7
Asif Syed <asiifsyed@gmail.com>
2025-02-17 00:14:58 -0800

    footer icon size decreased

--- src/app/components/Footer/index.tsx
+++ src/app/components/Footer/index.tsx
@@ -6,30 +6,30 @@ import Link from "next/link";
 export const Footer = () => {
 
   return (
-    <div className="fixed bottom-0 left-0 w-full bg-green-300 py-5 flex items-center justify-center gap-3">
+    <div className="fixed bottom-0 left-0 w-full bg-green-300 py-3 flex items-center justify-center gap-3">
       {/* TODO -> Resume */}
       <Link
         href="https://www.linkedin.com/in/sonasathar/"
         target="_blank"
-        className="p-2 rounded-full bg-green-100 flex items-center justify-center justify-center transform transition-transform duration-300 ease-in-out hover:rotate-12"
+        className="p-1.5 rounded-full bg-green-100 flex items-center justify-center justify-center transform transition-transform duration-300 ease-in-out hover:rotate-12"
       >
-        <HiDocumentText className="h-6 w-6 text-green-900" />
+        <HiDocumentText className="h-5 w-5 text-green-900" />
       </Link>
 
       <Link
         href="https://www.linkedin.com/in/sonasathar/"
         target="_blank"
-        className="p-2 rounded-full bg-green-100 flex items-center justify-center transform transition-transform duration-300 ease-in-out hover:rotate-12"
+        className="p-1.5 rounded-full bg-green-100 flex items-center justify-center transform transition-transform duration-300 ease-in-out hover:rotate-12"
       >
-        <RiLinkedinBoxFill className="h-6 w-6 text-green-900" />
+        <RiLinkedinBoxFill className="h-5 w-5 text-green-900" />
       </Link>
 
       <Link
         href="mailto:sonasathar@gmail.com"
         target="_blank"
-        className="p-2 rounded-full bg-green-100 flex items-center justify-center transform transition-transform duration-300 ease-in-out hover:rotate-12"
+        className="p-1.5 rounded-full bg-green-100 flex items-center justify-center transform transition-transform duration-300 ease-in-out hover:rotate-12"
       >
-        <HiMail className="h-6 w-6 text-green-900" />
+        <HiMail className="h-5 w-5 text-green-900" />
       </Link>
     </div>
   );
